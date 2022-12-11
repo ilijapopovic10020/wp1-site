8809169e163405ee6296f5196e74448b7777167a
ilijaPopovic01 <ilija.popovic.100.20@ict.edu.rs>
2022-12-11 17:12:59 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,8 @@ window.onload = function () {
     galleryWrite();
     menuWrite();
     authorLangsWrite();
+    scrollNav();
+    formWrite();
 }
 
 //Dinamicki ispis navigacije
@@ -73,9 +75,110 @@ function authorLangsWrite() {
 }
 
 function formWrite() {
-    
+    // Create a form dynamically
+    var form = document.querySelector("#registration")
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "submit.php");
+
+    // Create an input element for first Name
+    var fnInp = document.createElement("input");
+    var fnLbl = document.createElement("Label");
+    fnLbl.setAttribute("for", "firstName");
+    fnLbl.classList.add("form-label");
+    var fnTn = document.createTextNode("Ime");
+    fnLbl.appendChild(fnTn);
+    fnInp.setAttribute("type", "text");
+    fnInp.setAttribute("name", "firstName");
+    fnInp.setAttribute("placeholder", "Ime");
+    fnLbl.classList.add("form-label");
+
+    form.appendChild(fnLbl);
+    form.appendChild(fnInp);
+
+    console.log("asd")
+
+
+    // // Create an input element for last Name
+    // var lnInp = document.createElement("input");
+    // FN.setAttribute("type", "text");
+    // FN.setAttribute("name", "lastName");
+    // FN.setAttribute("placeholder", "Prezime");
+
+    // // Create an input element for date of birth
+    // var DOB = document.createElement("input");
+    // DOB.setAttribute("type", "text");
+    // DOB.setAttribute("name", "dob");
+    // DOB.setAttribute("placeholder", "DOB");
+
+    // // Create an input element for emailID
+    // var EID = document.createElement("input");
+    // EID.setAttribute("type", "text");
+    // EID.setAttribute("name", "emailID");
+    // EID.setAttribute("placeholder", "E-Mail ID");
+
+    // // Create an input element for password
+    // var PWD = document.createElement("input");
+    // PWD.setAttribute("type", "password");
+    // PWD.setAttribute("name", "password");
+    // PWD.setAttribute("placeholder", "Password");
+
+    // // Create an input element for retype-password
+    // var RPWD = document.createElement("input");
+    // RPWD.setAttribute("type", "password");
+    // RPWD.setAttribute("name", "reTypePassword");
+    // RPWD.setAttribute("placeholder", "ReEnter Password");
+
+    // // create a submit button
+    // var s = document.createElement("input");
+    // s.setAttribute("type", "submit");
+    // s.setAttribute("value", "Submit");
+
+    // // Append the full name input to the form
+    // form.appendChild(FN);
+
+    // // Inserting a line break
+    // form.appendChild(br.cloneNode());
+
+    // // Append the DOB to the form
+    // form.appendChild(DOB);
+    // form.appendChild(br.cloneNode());
+
+    // // Append the emailID to the form
+    // form.appendChild(EID);
+    // form.appendChild(br.cloneNode());
+
+    // // Append the Password to the form
+    // form.appendChild(PWD);
+    // form.appendChild(br.cloneNode());
+
+    // // Append the ReEnterPassword to the form
+    // form.appendChild(RPWD);
+    // form.appendChild(br.cloneNode());
+
+    // // Append the submit button to the form
+    // form.appendChild(s);
+
+    // document.getElementsByTagName("body")[0]
+    //     .appendChild(form);
 }
 
 function formValidation() {
 
+}
+
+window.onscroll = function () { scrollNav() };
+
+var header = document.querySelector("nav");
+var fixed = header.offsetTop;
+
+function scrollNav() {
+    if (window.pageYOffset > fixed) {
+        header.classList.add("fixed-top");
+        header.style.backgroundColor = "rgb(0, 0, 0, 0.75)";
+        header.style.transition = "0.3s";
+    } else {
+        header.classList.remove("fixed-top");
+        header.style.backgroundColor = "";
+        header.style.transtition = "0.3s";
+    }
 }
